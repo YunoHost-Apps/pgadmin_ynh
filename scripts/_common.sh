205b0028dc7fbd0ff82ea4ad81c9f31d71f3198c
Félix Piédallu <felix@piedallu.me>
2024-01-30 11:36:14 +0100

    update packagingv2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 python_version="$(python3 -V | cut -d' ' -f2 | cut -d. -f1-2)"
 
 # dependencies used by the app
-pkg_dependencies="python3-pip build-essential python3-dev python3-venv postgresql uwsgi uwsgi-plugin-python3 expect libpq-dev libkrb5-dev"
+#REMOVEME? pkg_dependencies="python3-pip build-essential python3-dev python3-venv postgresql uwsgi uwsgi-plugin-python3 expect libpq-dev libkrb5-dev"
 
 #=================================================
 # DEFINE ALL COMMON FONCTIONS
@@ -15,39 +15,39 @@ setup_dir() {
     # Create empty dir for pgadmin
     mkdir -p /var/lib/pgadmin
     mkdir -p /var/log/pgadmin
-    mkdir -p $final_path
+    mkdir -p $install_dir
 }
 
 install_source() {
     # Clean venv is it was on python with an old version in case major upgrade of debian
-    if [ ! -e $final_path/lib/python$python_version ]; then
-        ynh_secure_remove --file=$final_path/bin
-        ynh_secure_remove --file=$final_path/lib
-        ynh_secure_remove --file=$final_path/lib64
-        ynh_secure_remove --file=$final_path/include
-        ynh_secure_remove --file=$final_path/share
-        ynh_secure_remove --file=$final_path/pyvenv.cfg
+    if [ ! -e $install_dir/lib/python$python_version ]; then
+#REMOVEME?         ynh_secure_remove --file=$install_dir/bin
+#REMOVEME?         ynh_secure_remove --file=$install_dir/lib
+#REMOVEME?         ynh_secure_remove --file=$install_dir/lib64
+#REMOVEME?         ynh_secure_remove --file=$install_dir/include
+#REMOVEME?         ynh_secure_remove --file=$install_dir/share
+#REMOVEME?         ynh_secure_remove --file=$install_dir/pyvenv.cfg
     fi
 
-    mkdir -p $final_path
-    chown $pgadmin_user:root -R $final_path
+    mkdir -p $install_dir
+    chown $app:root -R $install_dir
 
     if [ -n "$(uname -m | grep arm)" ]
     then
         # Clean old file, sometime it could make some big issues if we don't do this !!
-        ynh_secure_remove --file=$final_path/bin
-        ynh_secure_remove --file=$final_path/lib
-        ynh_secure_remove --file=$final_path/include
-        ynh_secure_remove --file=$final_path/share
-        ynh_setup_source --dest_dir $final_path/ --source_id "armv7_$(lsb_release --codename --short)"
+#REMOVEME?         ynh_secure_remove --file=$install_dir/bin
+#REMOVEME?         ynh_secure_remove --file=$install_dir/lib
+#REMOVEME?         ynh_secure_remove --file=$install_dir/include
+#REMOVEME?         ynh_secure_remove --file=$install_dir/share
+        ynh_setup_source --dest_dir $install_dir/ --source_id "armv7_$(lsb_release --codename --short)"
     else
 # 		Install virtualenv if it don't exist
-        test -e $final_path/bin/python3 || python3 -m venv $final_path
+#REMOVEME?         test -e $install_dir/bin/python3 || python3 -m venv $install_dir
 
 # 		Install pgadmin in virtualenv
         u_arg='u'
         set +$u_arg;
-        source $final_path/bin/activate
+        source $install_dir/bin/activate
         set -$u_arg;
         pip3 install --upgrade pip wheel
         pip3 install -I --upgrade "psycopg[c]"
@@ -60,17 +60,17 @@ install_source() {
 
 set_permission() {
     # Set permission
-    chown $pgadmin_user:root -R $final_path
-    chown $pgadmin_user:root -R /var/lib/pgadmin
+    chown $app:root -R $install_dir
+    chown $app:root -R /var/lib/pgadmin
     mkdir -p /var/log/pgadmin
-    chown $pgadmin_user:root -R /var/log/pgadmin
-    chown $pgadmin_user:root /var/log/uwsgi/$app
-    chown $pgadmin_user:root /etc/uwsgi/apps-available/$app.ini
+    chown $app:root -R /var/log/pgadmin
+    chown $app:root /var/log/uwsgi/$app
+    chown $app:root /etc/uwsgi/apps-available/$app.ini
     chmod u=rwX,g=rX,o= -R /var/lib/pgadmin
 }
 
 config_pgadmin() {
-    cp ../conf/config_local.py $final_path/lib/python$python_version/site-packages/pgadmin4/config_local.py
-    ynh_replace_string --match_string __USER__ --replace_string $pgadmin_user --target_file $final_path/lib/python$python_version/site-packages/pgadmin4/config_local.py
-    ynh_replace_string --match_string __DOMAIN__ --replace_string $domain --target_file $final_path/lib/python$python_version/site-packages/pgadmin4/config_local.py
+    cp ../conf/config_local.py $install_dir/lib/python$python_version/site-packages/pgadmin4/config_local.py
+    ynh_replace_string --match_string __USER__ --replace_string $app --target_file $install_dir/lib/python$python_version/site-packages/pgadmin4/config_local.py
+    ynh_replace_string --match_string __DOMAIN__ --replace_string $domain --target_file $install_dir/lib/python$python_version/site-packages/pgadmin4/config_local.py
 }
