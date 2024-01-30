463bea48fd28373900b3d5847d7d09dab8f1c249
Félix Piédallu <felix@piedallu.me>
2024-01-30 16:48:42 +0100

    fix permission, continue

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -22,16 +22,24 @@ set_permission() {
     chown $app:root -R /var/lib/pgadmin
     mkdir -p /var/log/pgadmin
     chown $app:root -R /var/log/pgadmin
-    chown $app:root /var/log/uwsgi/$app
-    chown $app:root /etc/uwsgi/apps-available/$app.ini
     chmod u=rwX,g=rX,o= -R /var/lib/pgadmin
 }
 
-_install_pgadmin_venv() {
-    ynh_exec_as "$app" python3 -m venv --upgrade "$install_dir/venv"
-    venvpy="$install_dir/venv/bin/python3"
+ynh_install_venv() {
+    if [ -f "$install_dir/venv/bin/python" ]; then
+        ynh_exec_as "$app" python3 -m venv "$install_dir/venv" --upgrade
+    else
+        ynh_exec_as "$app" python3 -m venv "$install_dir/venv"
+    fi
+    ynh_use_venv
 
     ynh_exec_as "$app" "$venvpy" -m pip install --upgrade --no-cache-dir pip wheel
+}
+ynh_use_venv() {
+    venvpy="$install_dir/venv/bin/python3"
+}
+
+_install_pgadmin_pip() {
     ynh_exec_as "$app" "$venvpy" -m pip install --upgrade --no-cache-dir --ignore-installed "psycopg[c]"
 
     cp "$YNH_APP_BASEDIR/conf/requirement_$(lsb_release --codename --short).txt" "$install_dir/requirements.txt"
