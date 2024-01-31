66d63af2db06b566763a0eca682102753f6ee27d
Félix Piédallu <felix.piedallu@non.se.com>
2024-01-31 14:52:39 +0100

    Cleanup permissions

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,21 +10,6 @@ python_version="$(python3 -V | cut -d' ' -f2 | cut -d. -f1-2)"
 # DEFINE ALL COMMON FONCTIONS
 #=================================================
 
-setup_dir() {
-    # Create empty dir for pgadmin
-    mkdir -p /var/lib/pgadmin
-    mkdir -p /var/log/pgadmin
-}
-
-set_permission() {
-    # Set permission
-    chown $app:root -R $install_dir
-    chown $app:root -R /var/lib/pgadmin
-    mkdir -p /var/log/pgadmin
-    chown $app:root -R /var/log/pgadmin
-    chmod u=rwX,g=rX,o= -R /var/lib/pgadmin
-}
-
 ynh_install_venv() {
     if [ -f "$install_dir/venv/bin/python" ]; then
         ynh_exec_as "$app" python3 -m venv "$install_dir/venv" --upgrade
